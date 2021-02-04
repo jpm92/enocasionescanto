cc74a6e3e322f1bb15110517b677245022fee5e0
javiexe <javirodriguez95@gmail.com>
2021-02-04 02:26:46 +0100

    menu desplegable

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.js-open').on('click', function () {
+      $('html').addClass('menu-open');
+    });
+  
+    $('.js-close').on('click', function () {
+      $('html').removeClass('menu-open');
+    });
+});
