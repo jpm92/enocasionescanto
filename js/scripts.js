d464f098d8f1f78903de8e32eb6fad3adfc80e8c
javiexe <javirodriguez95@gmail.com>
2021-02-10 12:28:02 +0100

    botones java y arreglos varios

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,20 @@ $(document).ready(function () {
       $('html').removeClass('menu-open');
     });
 });
+
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 600) {
+     $('.minilogo').addClass('fondo');
+  } else {
+     $('.minilogo').removeClass('fondo');
+  }
+});
+
+$(window).scroll(function(){
+  if ($(this).scrollTop() > 20) {
+     $('.spotify_content').addClass('fondo');
+  } else {
+     $('.spotify_content').removeClass('fondo');
+  }
+});
